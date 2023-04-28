85db6cf4065759bae245b8eaf15ea23f93f97f45
Anderwander <ander1993trabajo@hotmail.com>
2023-04-28 18:40:06 +0200

    ahora está bien en teoría. y en teoría funciona hasta el comunismo... en teoría

---
+++ my-portfolio/src/components/Home/Data.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">Ander García</h1>
+      <h3 className="home__subtitle">Frontend Developer</h3>
+      <p className="home__description">
+        I'm a junior web developer from Bilbao, and i'm willing to start my
+        career as a web developer
+      </p>
+      <a href="#contact" className="button button--flex">
+        Contact Me!!
+      </a>
+    </div>
+  );
+};
+
+export default Data;
