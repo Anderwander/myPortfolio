173d0afa99468c049cb427ac56be2b7d19e67adf
Anderwander <ander1993trabajo@hotmail.com>
2023-05-02 00:02:20 +0200

    terminado portfolio

--- my-portfolio/src/components/About/Info.jsx
+++ my-portfolio/src/components/About/Info.jsx
@@ -6,19 +6,19 @@ const Info = () => {
       <div className="about__box">
         <i class="bx bx-award about__icon"></i>
         <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">Los años que lleve currandorl</span>
+        <span className="about__subtitle">Llevo 150años</span>
       </div>
 
       <div className="about__box">
         <i class="bx bxs-briefcase-alt about__icon"></i>
         <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">Pues 0 proyectos chiques</span>
+        <span className="about__subtitle">Mínimo 2</span>
       </div>
 
       <div className="about__box">
         <i class="bx bx-support about__icon"></i>
         <h3 className="about__title">Support</h3>
-        <span className="about__subtitle">Online 24/7 vas flipau</span>
+        <span className="about__subtitle">Los lunes de 8 a 9am</span>
       </div>
     </div>
   );
--- my-portfolio/src/components/Portfolio/Portfolio.jsx
+++ my-portfolio/src/components/Portfolio/Portfolio.jsx
@@ -7,6 +7,8 @@ const Portfolio = () => {
     <section className="work section" id="portfolio">
       <h2 className="section__title">Portfolio</h2>
       <span className="section__subtitle">Most recent works </span>
+
+      <Works></Works>
     </section>
   );
 };
